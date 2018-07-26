e008e0356b01abd498770651026f18032e6d9b97
anna-rose <917566@isd624.org>
2018-07-26 01:23:36 +0000

    automatically backed up by learn

---
+++ Fun.rb
@@ -0,0 +1,8 @@
+puts "who is the the greatest ping pong player/sexy artist in the world?"
+answer = gets.chomp
+puts "is it weird or cute when he makes a beat out of his hand farts?"
+cute = gets.chomp
+puts "should emma go with anna if he ever has a us tour?"
+yes = gets.chomp
+
+puts ""
